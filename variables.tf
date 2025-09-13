a6555dc6938e5166eb008af6d68e7a7adcb33076
Rashmi Patil <rashmipatil@Rashmis-Laptop.local>
2025-09-13 18:16:46 +0530

    Add Terraform infra files

--- variables.tf
+++ variables.tf
@@ -8,3 +8,15 @@ variable "region" {
   description = "GCP region to deploy resources in"
   default     = "us-central1"
 }
+
+variable "artifact_repo" {
+  description = "Artifact Registry repository name where container images are pushed"
+  type        = string
+  default     = "sample-repo"   # change this to your preferred repo name
+}
+
+variable "image_tag" {
+  description = "Image tag to deploy"
+  type        = string
+  default     = "v1"
+}
